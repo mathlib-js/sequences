2f9cd690b7013c6103d5b90354926c3a9c7ae2b5
Vivek Dinesh <vivekdinesh5@gmail.com>
2022-05-15 20:53:53 +0530

    Lucas Numbers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import fibonacci from "./fibonacci";
+import lucasNumbers from "./lucasNumbers";
 import naturalNumbers from "./naturalNumbers";
 import wholeNumbers from "./wholeNumbers";
 
 export {
   fibonacci,
+  lucasNumbers,
   naturalNumbers,
   wholeNumbers
 };
---
+++ src/lucasNumbers.ts
@@ -0,0 +1,16 @@
+function* lucasNumbers(limit = 10) {
+  let count = 0;
+  let a = 2;
+  let b = 1;
+
+  if (isNaN(limit) || limit < 1) {
+    throw new Error("Limit should be an integer greater than 0.")
+  }
+
+  while (count++ < limit) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+}
+
+export default lucasNumbers;
